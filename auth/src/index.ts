241ab6fd33159f167c425e8a6bccfd447e534547
Savan <sawanpatel2508@gmail.com>
2024-11-11 21:13:59 -0500

    Add mongodb auth kubernetes pod

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-import { json } from 'body-parser';
-import {currentUserRouter} from "./routs/current-user";
-import {signinRouter} from "./routs/signin";
-import {signoutRouter} from "./routs/signout";
-import {signupRouter} from "./routs/signup";
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import { currentUserRouter } from "./routs/current-user";
+import { signinRouter } from "./routs/signin";
+import { signoutRouter } from "./routs/signout";
+import { signupRouter } from "./routs/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +16,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
-	console.log('Listening on port 3000!!!');
+  console.log("Listening on port 3000!!!");
 });
