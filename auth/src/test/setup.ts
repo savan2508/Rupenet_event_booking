e8a2243541610e649c68585f4211e9035c98b0e5
Savan <sawanpatel2508@gmail.com>
2024-11-14 16:26:49 -0500

    Add tests for the auth

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,55 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  // eslint-disable-next-line no-undef
+  process.env.JWT_KEY = "asdf";
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+      await collection.deleteMany({});
+    }
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+export const signinTest = async () => {
+  const email = "test@email.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  return response.get("Set-Cookie");
+};
